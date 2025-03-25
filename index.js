1c9c8d3cafc4919f24f6d1acfdacc0bc1db780b4
JoanneOs <jowan.awsman@yahoo.com>
2025-03-25 13:59:21 -0500

    try catch

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import { central, db1, db2, db3, vault } from './databases.js';
+
+
+const dbs = { db1, db2, db3 };
+
+async function getUserData(id) {
+  try {
+    const [db, user] = await Promise.all([central(id), vault(id)]);
+    return { id, ...user, ...await dbs[db](id) };
+  } catch (e) {
+    throw Error(`Failed: ${e.message}`);
+  }
+}
