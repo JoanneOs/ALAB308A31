171cdf7e8ceb0ab8e037751e6ad0c868ef6c3782
JoanneOs <jowan.awsman@yahoo.com>
2025-03-25 13:57:11 -0500

    database function

--- databases.js
+++ databases.js
@@ -1,1 +1,9 @@
 //
+// Fake database functions
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+export async function central(id) {
+  await wait(100);
+  if (id < 1 || id > 10) throw Error("Invalid ID");
+  return id <= 4 ? "db1" : id <= 7 ? "db2" : "db3";
+}
